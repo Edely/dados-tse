34392e3aaef34cfdde04630230890b157e82f142
edely <edely.gomes@gmail.com>
2018-09-29 14:05:37 -0300

    adicionando em data frame

--- desafio-correio.py
+++ desafio-correio.py
@@ -18,18 +18,39 @@ import numpy as np
 
 #df_br = pd.read_csv('dados/consulta_cand_2018_BRASIL.csv', encoding='latin1', sep=';')
 df_ba = pd.read_csv('dados/consulta_cand_2018_BA.csv', encoding='latin1', sep=';')
+colunas = list(df_ba)
 
 #cargos_code = df_ba.CD_CARGO.unique()
 #cargos_desc = df_ba.DS_CARGO.unique()
 
 
 #separar DataFrames por cargo
-df_dep_est = pd.DataFrame()
-df_dep_fed = pd.DataFrame()
-df_gov = pd.DataFrame()
-df_v_gov = pd.DataFrame()
-df_sen = pd.DataFrame()
-df_pri_sup = pd.DataFrame()
-df_seg_sup = pd.DataFrame()
+df_dep_est = pd.DataFrame(columns=colunas)
+df_dep_fed = pd.DataFrame(columns=colunas)
+df_gov = pd.DataFrame(columns=colunas)
+df_v_gov = pd.DataFrame(columns=colunas)
+df_sen = pd.DataFrame(columns=colunas)
+df_pri_sup = pd.DataFrame(columns=colunas)
+df_seg_sup = pd.DataFrame(columns=colunas)
 
-for i, row in df_ba():
+count = 0
+
+def add_row(row, colunas):
+    cargo = row['CD_CARGO']
+    df_row = pd.DataFrame([row], columns=colunas)
+
+    if(cargo == 3):        
+        df_dep_est.append(dict(row), ignore_index=True)
+    elif(cargo == 4):
+        df_v_gov.append(dict(row), ignore_index=True)
+    if(cargo == 5):        
+        df_dep_est.append(dict(row), ignore_index=True)
+    if(cargo == 4):
+        df_v_gov.append(dict(row), ignore_index=True)
+
+for i, row in df_ba.iterrows():
+    count += 1
+    add_row(row, colunas)
+    
+
+
