b1a355c13b620cc0e04098044539ef3143094332
edely <edely.gomes@gmail.com>
2018-10-01 07:50:51 -0300

    retornando os valores das colunas

--- script.py
+++ script.py
@@ -8,8 +8,34 @@ df_ba = pd.read_csv('dados/consulta_cand_2018_BA.csv', encoding='latin1', sep=';
 df_dict = df_ba.to_dict('index')
 
 #traz a porcentagem
-def percentage(the_column, the_data=df_dict):
-    #print(df_dict)
-    
+def distribution(the_data, *the_columns):
+    '''
+    Retorna a distribuicao de candidatos pelo critério selecionado (colunas)
+    '''
 
-percentage('NR_PROCESSO')
+    total_candidatos = 0
+
+    base = the_columns[0]
+    compare_to = the_columns[1]
+
+
+    values_base = set()
+    values_compare = set()
+
+    for key, candidato in the_data.items():
+        values_base.add(candidato[base])
+        values_compare.add(candidato[compare_to])
+        total_candidatos +=1
+
+    #print(values_base)
+    #print(values_compare)
+    #print(total_candidatos)
+
+    for k, v in the_data.items():
+        for column in the_columns:
+            
+            #print('\n')
+            #print(v['NM_CANDIDATO']+ ' ' + v[base] + ' ' + v[compare_to] +'\n')
+
+
+distribution(df_dict, 'DS_GRAU_INSTRUCAO', 'DS_GENERO')
